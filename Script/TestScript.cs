6c6c1af3d1881a4457f857c983131364f95734a5
WCCB <tfb00plzdj@gmail.com>
2016-01-23 12:13:30 +0900

    Excelの配置を変更

---
+++ Script/TestScript.cs
@@ -0,0 +1,24 @@
+﻿using AscheLib;
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+
+public class TestScript : AscheBehaviour {
+
+	public UnityEngine.UI.Image image = null;
+
+	//開始時
+	public void Start () {
+		ResourceLoader.Instance.LoadSpriteObservable("Asche_CancelButton")
+			.Subscribe(resource => {
+				image.sprite = resource;
+			});
+	}
+
+	//更新処理
+	public void Update () {
+	
+	}
+}
